13a2c71b51d856ffd4d62a7c39f22b5b28c1d035
gamesholplays <98983006+gamesholplays@users.noreply.github.com>
2026-06-12 02:21:02 +0300

    Update index.ts

--- plugins/DoubleTapReply/src/index.ts
+++ plugins/DoubleTapReply/src/index.ts
@@ -1,25 +1,9 @@
-import { findByProps } from "@vendetta/metro";
+import { findByProps, findByStoreName } from "@vendetta/metro";
 
 export default {
     onLoad() {
-        const props = [
-            "handleTouchStart",
-            "onResponderGrant", 
-            "setGestureState",
-            "GestureHandler",
-            "TapGestureHandler",
-            "onSingleTap",
-            "onDoubleTap",
-            "numberOfTaps",
-            "LongPressGestureHandler",
-        ];
-
-        let found = "";
-        for (const prop of props) {
-            const mod = findByProps(prop);
-            if (mod) found += prop + ", ";
-        }
-        alert("[DTR] Found: " + (found || "NONE"));
+        const GestureHandler = findByProps("TapGestureHandler");
+        alert("[DTR] TapGestureHandler keys: " + Object.keys(GestureHandler).join(", "));
     },
     onUnload() {},
 };
