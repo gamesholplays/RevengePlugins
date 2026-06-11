e5c8bc2f9dc2d8edd84fe24d2068bc4d08d24c6b
gamesholplays <98983006+gamesholplays@users.noreply.github.com>
2026-06-12 02:22:52 +0300

    Update index.ts

--- plugins/DoubleTapReply/src/index.ts
+++ plugins/DoubleTapReply/src/index.ts
@@ -1,9 +1,21 @@
-import { findByProps, findByStoreName } from "@vendetta/metro";
+import { findByProps } from "@vendetta/metro";
 
 export default {
     onLoad() {
-        const GestureHandler = findByProps("TapGestureHandler");
-        alert("[DTR] TapGestureHandler keys: " + Object.keys(GestureHandler).join(", "));
+        const FluxDispatcher = findByProps("dispatch", "subscribe");
+        const orig = FluxDispatcher.dispatch.bind(FluxDispatcher);
+
+        FluxDispatcher.dispatch = (event: any) => {
+            if (event?.type && !event.type.includes("HEARTBEAT") && !event.type.includes("TRACK")) {
+                console.log("[DTR]", event.type, JSON.stringify(event).slice(0, 100));
+            }
+            return orig(event);
+        };
+
+        (this as any)._orig = orig;
+        (this as any)._fd = FluxDispatcher;
     },
-    onUnload() {},
+    onUnload() {
+        if ((this as any)._fd) (this as any)._fd.dispatch = (this as any)._orig;
+    },
 };
