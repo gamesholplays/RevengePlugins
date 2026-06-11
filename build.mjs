c4e616b5639a5fac38ef4ceabe02c91b3cd1d9e7
gamesholplays <98983006+gamesholplays@users.noreply.github.com>
2026-06-11 23:09:50 +0300

    Add files via upload

--- build.mjs
+++ build.mjs
@@ -1,29 +1,17 @@
 import { readFile, writeFile, readdir } from "fs/promises";
-import path, { extname } from "path";
+import { extname } from "path";
 import { createHash } from "crypto";
-
 import { rollup } from "rollup";
 import esbuild from "rollup-plugin-esbuild";
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import alias from "@rollup/plugin-alias";
 import swc from "@swc/core";
-import { fileURLToPath } from "url";
 
 const extensions = [".js", ".jsx", ".mjs", ".ts", ".tsx", ".cts", ".mts"];
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type import("rollup").InputPluginOption */
 const plugins = [
-    alias({
-        entries: [
-            {
-                find: "@lib", replacement: path.resolve(__dirname, "lib")
-            }
-        ]
-    }),
-    nodeResolve({ extensions }),
+    nodeResolve(),
     commonjs(),
     {
         name: "swc",
@@ -53,6 +41,7 @@ const plugins = [
                     ],
                 },
             });
+
             return result.code;
         },
     },
@@ -69,7 +58,7 @@ for (let plug of await readdir("./plugins")) {
             onwarn: () => {},
             plugins,
         });
-    
+
         await bundle.write({
             file: outPath,
             globals(id) {
@@ -77,20 +66,20 @@ for (let plug of await readdir("./plugins")) {
                 const map = {
                     react: "window.React",
                 };
-
                 return map[id] || null;
             },
             format: "iife",
             compact: true,
             exports: "named",
         });
+
         await bundle.close();
-    
+
         const toHash = await readFile(outPath);
         manifest.hash = createHash("sha256").update(toHash).digest("hex");
         manifest.main = "index.js";
+
         await writeFile(`./dist/${plug}/manifest.json`, JSON.stringify(manifest));
-    
         console.log(`Successfully built ${manifest.name}!`);
     } catch (e) {
         console.error("Failed to build plugin...", e);
