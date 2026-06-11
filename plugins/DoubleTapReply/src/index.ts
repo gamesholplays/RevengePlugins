b9096e3b0fb4ab7712981bca08ebc71fd202a553
gamesholplays <98983006+gamesholplays@users.noreply.github.com>
2026-06-12 02:02:19 +0300

    Update index.ts

--- plugins/DoubleTapReply/src/index.ts
+++ plugins/DoubleTapReply/src/index.ts
@@ -1,14 +1,24 @@
-import { findByProps } from "@vendetta/metro";
+import { findByName } from "@vendetta/metro";
 
 export default {
     onLoad() {
-        const onPressModule = findByProps("onPress");
-        const keys = onPressModule ? Object.keys(onPressModule).join(", ") : "none";
-        alert("[DoubleTapReply] onPress module keys: " + keys);
+        const names = [
+            "Message",
+            "MessageItem",
+            "ChatMessage",
+            "BaseMessage",
+            "MessageContent",
+            "ChannelMessage",
+            "MessageListItem",
+            "MessageWrapper",
+        ];
 
-        const onLongPressModule = findByProps("onLongPress");
-        const keys2 = onLongPressModule ? Object.keys(onLongPressModule).join(", ") : "none";
-        alert("[DoubleTapReply] onLongPress module keys: " + keys2);
+        let found = "";
+        for (const name of names) {
+            const mod = findByName(name, false);
+            if (mod) found += name + ", ";
+        }
+        alert("[DoubleTapReply] Found by name: " + (found || "NONE"));
     },
 
     onUnload() {},
