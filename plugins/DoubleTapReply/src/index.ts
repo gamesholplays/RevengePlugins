f75d068963a6ea13053cfd71d3c743c12e6c6a5b
gamesholplays <98983006+gamesholplays@users.noreply.github.com>
2026-06-12 13:43:20 +0300

    Update index.ts

--- plugins/DoubleTapReply/src/index.ts
+++ plugins/DoubleTapReply/src/index.ts
@@ -12,17 +12,9 @@ export default {
         }
 
         const FluxDispatcher = findByProps("_interceptors", "_subscriptions");
-        let actionSheetOpen = false;
-
-        const sheetInterceptor = (event: any) => {
-            if (event?.type === "SHOW_ACTION_SHEET") actionSheetOpen = true;
-            if (event?.type === "HIDE_ACTION_SHEET") actionSheetOpen = false;
-            return false;
-        };
 
         const interceptor = (event: any) => {
             if (event?.type !== "MESSAGE_REACTION_ADD") return false;
-            if (actionSheetOpen) return false;
 
             const { channelId, messageId } = event;
             if (!channelId || !messageId) return false;
@@ -40,20 +32,16 @@ export default {
             return true;
         };
 
-        FluxDispatcher._interceptors.push(sheetInterceptor);
         FluxDispatcher._interceptors.push(interceptor);
         (this as any)._interceptor = interceptor;
-        (this as any)._sheetInterceptor = sheetInterceptor;
         (this as any)._dispatcher = FluxDispatcher;
     },
 
     onUnload() {
-        const { _interceptor, _sheetInterceptor, _dispatcher } = this as any;
-        if (_dispatcher) {
-            [_interceptor, _sheetInterceptor].forEach(i => {
-                const idx = _dispatcher._interceptors.indexOf(i);
-                if (idx !== -1) _dispatcher._interceptors.splice(idx, 1);
-            });
+        const { _interceptor, _dispatcher } = this as any;
+        if (_dispatcher && _interceptor) {
+            const idx = _dispatcher._interceptors.indexOf(_interceptor);
+            if (idx !== -1) _dispatcher._interceptors.splice(idx, 1);
         }
     },
 };
